295c2d2fbb6a4584010b24756b7748720fbff227
Leo Ciardi <leolopes.ciardi04@gmail.com>
2026-08-26 14:34:26 -0300

    feat: criei uma endpoint aberta /test para testar a API visualmente (ainda precisa de algumas melhorias

--- src/main/java/com/humanin/planpaz/controller/TestController.java
+++ src/main/java/com/humanin/planpaz/controller/TestController.java
@@ -3,16 +3,10 @@ package com.humanin.planpaz.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.humanin.planpaz.model.User;
-
 @Controller
 public class TestController {
 	@GetMapping("/test")
 	public String test() {
-		// testing lombok
-		User user = new User();
-		user.setEmail("aa@gmail.com");
-		System.out.println(user.getEmail());
-		return "a";
+		return "forward:/test.html";
 	}
 }
